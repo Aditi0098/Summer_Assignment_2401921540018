7f6f6875c3109ae922bbbb8319c41c3c369a350a
Aditi Bhardwaj <aditi2006bhardwaj@gmail.com>
2026-06-01 22:46:28 +0530

    Implement method to calculate maximum profit from stock prices

--- Week_1/Day_1/BestTimeToBuyAndSellStock.java
+++ Week_1/Day_1/BestTimeToBuyAndSellStock.java
@@ -1,1 +1,22 @@
+class Solution {
+
+    public int mp(int[] prices) {
+
+        int mp = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+
+            for (int j = i + 1; j < prices.length; j++) {
+
+                int profit = prices[j] - prices[i];
+
+                if (profit > mp) {
+                    mp = profit;
+                }
+            }
+        }
+
+        return mp;
+    }
+}
 
