e894df31c83ed99838a2456a759442f1e0278fe5
Aditi Bhardwaj <aditi2006bhardwaj@gmail.com>
2026-06-01 22:55:03 +0530

    Update BestTimeToBuyAndSellStock.java

--- Week_1/Day_1/BestTimeToBuyAndSellStock.java
+++ Week_1/Day_1/BestTimeToBuyAndSellStock.java
@@ -1,22 +1,15 @@
 class Solution {
-
-    public int mp(int[] prices) {
-
-        int mp = 0;
-
-        for (int i = 0; i < prices.length; i++) {
-
-            for (int j = i + 1; j < prices.length; j++) {
-
-                int profit = prices[j] - prices[i];
-
-                if (profit > mp) {
-                    mp = profit;
-                }
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for(int price : prices){
+            if(price < minPrice){
+                minPrice = price;
+            }
+            else{
+                maxProfit = Math.max(maxProfit, price - minPrice);
             }
         }
-
-        return mp;
+        return maxProfit;
     }
 }
-
