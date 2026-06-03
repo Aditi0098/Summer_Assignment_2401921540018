8b7745e6b9eec29df13bc8e1346ecdaa9a40285c
Aditi Bhardwaj <aditi2006bhardwaj@gmail.com>
2026-06-03 22:36:23 +0530

    Update ContainerWithMostWater.java

--- Week_1/Day_3/ContainerWithMostWater.java
+++ Week_1/Day_3/ContainerWithMostWater.java
@@ -1,1 +1,18 @@
-
+class Solution {
+    public int maxArea(int[] height) {
+        int maxwater =0;
+        int lp =0, rp = height.length-1;
+        while(lp<rp){
+           int w = rp-lp;
+           int ht = Math.min(height[lp],height[rp]);
+           int currWater = w*ht;
+           maxwater = Math.max(maxwater, currWater);
+           if (height[lp] < height[rp]) {
+           lp++;
+         } else {
+            rp--;
+         }
+        }
+        return maxwater;
+    }
+}
