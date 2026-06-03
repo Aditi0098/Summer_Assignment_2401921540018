6129fad1cbe3cb6d7daeb0e5d0813d8f2987e387
Aditi Bhardwaj <aditi2006bhardwaj@gmail.com>
2026-06-03 22:52:21 +0530

    Update Squareofsortedarray.java

--- Week_1/Day_3/Squareofsortedarray.java
+++ Week_1/Day_3/Squareofsortedarray.java
@@ -1,1 +1,21 @@
-
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int[] result = new int[nums.length];
+        for(int i =0;i<nums.length;i++){
+            nums[i] = nums[i] * nums[i];
+        }
+        int front = 0;
+        int rear = nums.length -1;
+        for(int j =nums.length-1;j>=0;j--){
+            if(nums[front] > nums[rear]){
+            result[j] = nums[front];
+            front++;
+        }
+        else{
+            result[j] = nums[rear];
+            rear--;
+        }
+    }
+    return result;
+}
+}
