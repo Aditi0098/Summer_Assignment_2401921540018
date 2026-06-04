e48b59c6dabe2c8fa03c0ce3f46373eb86420a2e
Aditi Bhardwaj <aditi2006bhardwaj@gmail.com>
2026-06-04 22:26:55 +0530

    Update ReshapetheMatrix.java

--- Week_1/Day_4/ReshapetheMatrix.java
+++ Week_1/Day_4/ReshapetheMatrix.java
@@ -1,1 +1,18 @@
+class Solution {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        int m = mat.length;
+        int n = mat[0].length;
+        if(m*n != r*c) return mat;
+        if(r==m && c==n) return mat;
+        int newind = 0;
+        int oldind = 0;
+        int newmat[][] = new int [r][c];
+        while(newind <r*c && oldind < m*n){
+            newmat[newind/c][newind%c] = mat[oldind/n][oldind%n];
+            newind++;
+            oldind++;
+        }
+        return newmat;
 
+    }
+}
