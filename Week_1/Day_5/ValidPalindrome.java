2f9fa5a0baeec8f20d2a5b0641ab74e912d11f28
Aditi Bhardwaj <aditi2006bhardwaj@gmail.com>
2026-06-05 22:57:34 +0530

    Update ValidPalindrome.java

--- Week_1/Day_5/ValidPalindrome.java
+++ Week_1/Day_5/ValidPalindrome.java
@@ -1,1 +1,27 @@
-
+class Solution {
+  public  boolean isAlphaNum(char ch){
+        if((ch >= '0' && ch <= '9') || (Character.toLowerCase(ch) >= 'a' && Character.toLowerCase(ch) <= 'z')){
+        return true;
+        }
+        return false;
+    }
+    public boolean isPalindrome(String s) {
+       int srt =0, end = s.length()-1;
+       while(srt < end){
+        if(!isAlphaNum(s.charAt(srt))){
+            srt++;
+            continue;
+        }
+        if(!isAlphaNum(s.charAt(end))){
+            end--;
+            continue;
+        }
+       if (Character.toLowerCase(s.charAt(srt)) != Character.toLowerCase(s.charAt(end))) {
+            return false;
+        }
+        srt++;
+        end--;
+       }
+       return true;
+    }
+}
