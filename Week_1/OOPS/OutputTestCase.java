2b8870c42fbd94f8f4600fcf949772669a7ae1f3
Aditi Bhardwaj <aditi2006bhardwaj@gmail.com>
2026-06-06 16:58:49 +0530

    Update OutputTestCase.java

--- Week_1/OOPS/OutputTestCase.java
+++ Week_1/OOPS/OutputTestCase.java
@@ -1,1 +1,12 @@
+Kid User Test
+You have successfully registered under a kids account
+Sorry, Age must be less than 12 to register as a kid
+Book Issued successfully, please return the book within 10 days
+Oops, you are allowed to take only kids books
+
+Adult User Test
+Sorry, Age must be greater than 12 to register as an adult
+You have successfully registered under an Adult Account
+Oops, you are allowed to take only adult Fiction Books
+Book Issued successfully, please return the book within 7 days
 
