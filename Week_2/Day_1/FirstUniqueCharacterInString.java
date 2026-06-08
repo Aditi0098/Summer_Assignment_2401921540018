cc72788ffcb6279dfd4887b00cb258c8b9cfc676
Aditi Bhardwaj <aditi2006bhardwaj@gmail.com>
2026-06-08 22:44:28 +0530

    Update FirstUniqueCharacterInString.java

--- Week_2/Day_1/FirstUniqueCharacterInString.java
+++ Week_2/Day_1/FirstUniqueCharacterInString.java
@@ -1,1 +1,16 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        int[] freq = new int[26];
+        char[] s1 = s.toCharArray();
+        for(char value : s1){
+            freq[value-'a']++;
+        }
 
+        for(int i = 0;i<s.length();i++){
+            if(freq[s1[i]- 'a'] ==1){
+               return i;
+            }
+        }
+        return -1;
+    }
+}
