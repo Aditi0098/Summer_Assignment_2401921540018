34decbf33e517f85ab588cb16047c02023a54c27
Aditi Bhardwaj <aditi2006bhardwaj@gmail.com>
2026-06-08 22:53:36 +0530

    Update RansomNote.java

--- Week_2/Day_1/RansomNote.java
+++ Week_2/Day_1/RansomNote.java
@@ -1,1 +1,14 @@
-
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] digits = new int[26];
+        for(char value : magazine.toCharArray()){
+            digits[value-'a']++;
+        }
+        for(char value : ransomNote.toCharArray()){
+            digits[value-'a']--;
+            if(digits[value-'a']< 0)
+            return false;
+        }
+        return true;
+    }
+}
