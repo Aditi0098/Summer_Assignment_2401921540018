c1f838cbba412d18b37cb5b1874fc1bcad386e6b
Aditi Bhardwaj <aditi2006bhardwaj@gmail.com>
2026-06-08 22:33:19 +0530

    Update ValidAnalog.java

--- Week_2/Day_1/ValidAnalog.java
+++ Week_2/Day_1/ValidAnalog.java
@@ -1,1 +1,19 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        int[] digits = new int[26];
+           for(int i =0; i<s.length();i++){
+            digits[s.charAt(i) - 'a']++;
+            digits[t.charAt(i) -'a']--;
+           }
+           for(int value : digits){
+            if(value !=0){
+                return false;
+            }
+         }
+           return true;
+     }
+}
 
