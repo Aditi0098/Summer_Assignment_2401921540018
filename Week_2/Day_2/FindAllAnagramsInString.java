61cf9f9b04ed457c68b63bd8d3c52e7cd87be13e
Aditi Bhardwaj <aditi2006bhardwaj@gmail.com>
2026-06-09 23:11:22 +0530

    Update FindAllAnagramsInString.java

--- Week_2/Day_2/FindAllAnagramsInString.java
+++ Week_2/Day_2/FindAllAnagramsInString.java
@@ -1,1 +1,20 @@
-
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        int[] gl = new int[26];
+        int[] bajaj = new int[26];
+        List<Integer> result = new java.util.ArrayList<>();
+        for(char value: p.toCharArray()){
+            gl[value - 'a']++;
+        }
+        for(int i =0;i<s.length();i++){
+            bajaj[s.charAt(i) - 'a']++;
+            if(i>=p.length()){
+            bajaj[s.charAt(i-p.length()) -'a']--;
+            }
+            if(Arrays.equals(bajaj,gl)){
+                result.add(i-p.length()+1);
+            }
+        }
+        return result;
+    }
+}
