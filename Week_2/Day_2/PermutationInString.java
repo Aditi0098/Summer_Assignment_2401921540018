ca7e699e854d82dc39ce525081e2ebfe630fb6a0
Aditi Bhardwaj <aditi2006bhardwaj@gmail.com>
2026-06-09 22:38:20 +0530

    Update PermutationInString.java

--- Week_2/Day_2/PermutationInString.java
+++ Week_2/Day_2/PermutationInString.java
@@ -1,1 +1,30 @@
-
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int a = s1.length();
+        int b = s2.length();
+        if(b<a) return false;
+        int map1[] = new int[26];
+        int map2[] = new int[26];
+        for(int i = 0;i<a;i++){
+            map1[s1.charAt(i) - 'a']++;
+            map2[s2.charAt(i) - 'a']++;
+        }
+        if(isSame(map1,map2)){
+            return true;
+        }
+        for(int i = 1;i<=b-a;i++){
+            map2[s2.charAt(i-1) - 'a']--;
+            map2[s2.charAt(i+a-1) - 'a']++;
+            if(isSame(map1,map2)){
+                return true;
+            }
+        }
+        return false;
+    }
+    private boolean isSame (int map1[],int map2[]){
+        for(int i =0;i<26;i++){
+            if(map1[i] != map2[i]) return false;
+        }
+        return true;
+    }
+}
