ea4f7ca96b2747e93e0b6fee6c60664af9e5f894
Aditi Bhardwaj <aditi2006bhardwaj@gmail.com>
2026-06-10 23:10:29 +0530

    Update RepeatedSubstringPattern.java

--- Week_2/Day_3/RepeatedSubstringPattern.java
+++ Week_2/Day_3/RepeatedSubstringPattern.java
@@ -1,1 +1,16 @@
-
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        int a= s.length();
+        for(int i =a/2;i>=1;i--){
+            if(a%i ==0){
+                String gl = s.substring(0,i);
+                StringBuilder bajaj = new StringBuilder();
+                for(int j =1;j<=a/i;j++)
+                bajaj.append(gl);
+                if(bajaj.toString().equals(s))
+                return true;
+            }
+        }
+        return false;
+    }
+}
