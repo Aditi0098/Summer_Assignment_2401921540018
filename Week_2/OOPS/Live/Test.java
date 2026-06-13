10a2a6984cac0a2b7121db70c79af18db65ee750
Aditi Bhardwaj <aditi2006bhardwaj@gmail.com>
2026-06-13 22:24:12 +0530

    Update Test.java

--- Week_2/OOPS/Live/Test.java
+++ Week_2/OOPS/Live/Test.java
@@ -1,3 +1,8 @@
+package live;
+import music.Playable;
+import music.string.Veena;
+import music.wind.Saxophone;
+public class Test{
 public static void main(String[] args){
   Veena v = new Veena();
   v.play();
@@ -6,3 +11,7 @@ public static void main(String[] args){
   Playable p;
   p=v;
   p.play();
+  p=s;
+  p.play();
+ }
+}
