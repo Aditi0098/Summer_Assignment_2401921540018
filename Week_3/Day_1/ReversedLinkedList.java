f54e999aa86d6660376e4f6981581c31412ed1eb
Aditi Bhardwaj <aditi2006bhardwaj@gmail.com>
2026-06-15 22:22:12 +0530

    Update ReversedLinkedList.java

--- Week_3/Day_1/ReversedLinkedList.java
+++ Week_3/Day_1/ReversedLinkedList.java
@@ -1,1 +1,16 @@
-
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        Stack<Integer> valueStack = new Stack<>();
+        while(head != null){
+            valueStack.push(head.val);
+            head = head.next;
+        }
+        ListNode reversedList = new ListNode(Integer.MIN_VALUE);
+        ListNode ptr = reversedList;
+        while(!valueStack.isEmpty()){
+            ptr.next = new ListNode(valueStack.pop());
+            ptr=ptr.next;
+        }
+        return reversedList.next;
+    }
+}
