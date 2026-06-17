4d5579f379d53c9dfcee4ea0c316a029a37f4e2a
Aditi Bhardwaj <aditi2006bhardwaj@gmail.com>
2026-06-17 23:49:17 +0530

    Update NextGreaterElement1.java

--- Week_3/Day_3/NextGreaterElement1.java
+++ Week_3/Day_3/NextGreaterElement1.java
@@ -1,1 +1,16 @@
-
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        HashMap<Integer , Integer> gl = new HashMap();
+        Stack<Integer> stack = new Stack();
+        for(Integer num : nums2){
+            while(!stack.isEmpty() && stack.peek() < num){
+                gl.put(stack.pop(),num);
+            }
+            stack.push(num);
+        }
+        for(int i =0 ;i<nums1.length;i++){
+            nums1[i]= gl.getOrDefault(nums1[i],-1);
+        }
+        return nums1;
+ }
+}
