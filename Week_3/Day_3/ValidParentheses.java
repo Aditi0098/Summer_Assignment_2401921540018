62ca3515ce014464f326c59b563d523f3ad6d79f
Aditi Bhardwaj <aditi2006bhardwaj@gmail.com>
2026-06-17 23:34:10 +0530

    Update ValidParentheses.java

--- Week_3/Day_3/ValidParentheses.java
+++ Week_3/Day_3/ValidParentheses.java
@@ -1,1 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        char[] arr = new char[s.length()];
+        int top = -1;
+        for(char c : s.toCharArray()) {
+            if(c == '(' || c =='{' || c == '['){
+                arr[++top] = c;
+            }
+            else{
+                if (top == -1) return false;
+
+                char last = arr[top--];
+                if((c == ')' && last !='(') ||
+                ( c == '}' && last  !='{') ||
+                 ( c == ']' && last  !='[')) {
+                    return false;
+                 }
+            }
+        }
+        return top == -1;
+    }
+}
 
