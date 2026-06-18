481b916caedbdc93262efe5776f2dd4c4a703c5c
Aditi Bhardwaj <aditi2006bhardwaj@gmail.com>
2026-06-18 22:55:04 +0530

    Update LargestRectangleInHistogram.java

--- Week_3/Day_4/LargestRectangleInHistogram.java
+++ Week_3/Day_4/LargestRectangleInHistogram.java
@@ -1,1 +1,22 @@
-
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+        int gl = 0;
+        int n = heights.length;
+        for(int i =0;i<=n;i++){
+            int bajaj = (i == n) ? 0 : heights[i];
+            while(!stack.isEmpty() && bajaj < heights[stack.peek()]){
+                int height = heights[stack.pop()];
+                int width;
+                if(stack.isEmpty()){
+                    width = i;
+                }else{
+                    width = i - stack.peek() -1;
+                }
+                gl = Math.max(gl , height *width);
+            }
+            stack.push(i);
+        }
+        return gl;
+    }
+}
