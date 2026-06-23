7b4122e43a483f5aef33c69a7cbe9dfa7868c51d
Aditi Bhardwaj <aditi2006bhardwaj@gmail.com>
2026-06-24 00:49:07 +0530

    Update MaximumDepthOfBinaryTree.java

--- Week_4/Day_1/MaximumDepthOfBinaryTree.java
+++ Week_4/Day_1/MaximumDepthOfBinaryTree.java
@@ -1,1 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int levels = 0;
+        while(!queue.isEmpty()){
+            int nodelevel = queue.size();
+            while(nodelevel > 0){
+                TreeNode element = queue.poll();
+                if(element.left != null)
+                 queue.add(element.left);
+                if(element.right != null)
+                 queue.add(element.right);
 
+                nodelevel--;
+            }
+            levels++;
+        }
+        return levels;
+        
+    }
+}
