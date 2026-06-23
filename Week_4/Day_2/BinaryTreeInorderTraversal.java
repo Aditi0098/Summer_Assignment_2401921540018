5d5b2784d8e3b0745afff0ca41a957eec816f7db
Aditi Bhardwaj <aditi2006bhardwaj@gmail.com>
2026-06-24 00:01:46 +0530

    Update BinaryTreeInorderTraversal.java

--- Week_4/Day_2/BinaryTreeInorderTraversal.java
+++ Week_4/Day_2/BinaryTreeInorderTraversal.java
@@ -1,1 +1,40 @@
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private void ans(TreeNode root, List<Integer> res){
+        if(root == null){
+            return;
+        }
+        ans(root.left,res);
+        res.add(root.val);
+        ans(root.right,res);
+    }
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList();
+        Stack<TreeNode> stack = new Stack();
+        TreeNode curr = root;
+        while(curr != null || !stack.isEmpty()){
+            while(curr != null){
+            stack.push(curr);
+            curr = curr.left;
+            }
+        curr = stack.pop();
+        res.add(curr.val);
+        curr = curr.right; 
+        }
+    return res;
+    }
+}
