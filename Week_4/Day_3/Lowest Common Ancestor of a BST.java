67c566dfbee7e0a00daf0009179ad25a9b2a21b0
Aditi Bhardwaj <aditi2006bhardwaj@gmail.com>
2026-06-24 23:58:37 +0530

    Update Lowest Common Ancestor of a BST.java

--- Week_4/Day_3/Lowest Common Ancestor of a BST.java
+++ Week_4/Day_3/Lowest Common Ancestor of a BST.java
@@ -1,1 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        int min =   Math.min(p.val,q.val),max = Math.max(p.val,q.val);
+        while(root != null){
+            int curr = root.val;
+            if(min<curr && max < curr){
+                root = root.left;
+            }else if (min< curr && max<curr){
+                root = root.right;
+            }else{
+                break;
+            }
+        }
+        return root;
+    }
+}
