1d3e74be78d5b3cb9d28e9bb1fdc72895fd303dc
Aditi Bhardwaj <aditi2006bhardwaj@gmail.com>
2026-06-25 23:47:22 +0530

    Update Binary Tree Maximum Path Sum.java

--- Week_4/Day_4/Binary Tree Maximum Path Sum.java
+++ Week_4/Day_4/Binary Tree Maximum Path Sum.java
@@ -1,1 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int maxsum = Integer.MIN_VALUE;
+    private int solve(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        int left = Math.max(0,solve(root.left));
+        int right = Math.max(0,solve(root.right));
+        int curr = left + right + root.val;
+        maxsum = Math.max(maxsum,curr);
+        return root.val + Math.max(left,right);
 
+    }
+    public int maxPathSum(TreeNode root) {
+        solve(root);
+        return maxsum;       
+    }
+}
